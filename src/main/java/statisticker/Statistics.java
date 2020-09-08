1bae60d6440628c3281b46c022f105b67b664293
parthgoyal14 <33154302+parthgoyal14@users.noreply.github.com>
2020-09-08 11:01:56 +0530

    Update Statistics.java

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -1,10 +1,30 @@
 package statisticker;
 
-import java.util.List;
+import java.util.*;
 
 public class Statistics 
 {
+    static class Stats{
+        public Float min=Float.NaN;
+        public Float max=Float.NaN;
+        public Float avg=Float.NaN;
+    }
+    
     public static Stats getStatistics(List<___> numbers) {
         //implement the computation of statistics here
+        Statistics.Stats stat = new Stats();
+        if(numbers.isEmpty())
+        {
+            return stat;
+        }
+        Float s = 0f;
+        stat.min = Collections.min(numbers);
+        stat.max = Collections.max(numbers);
+        for (Float n:numbers)
+        {
+            s+=n;
+        }
+        stat.avg = s/numbers.size();
     }
+    return stat;
 }
